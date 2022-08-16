aca122c6eb8ddaa2965d13abc14b76b31af6fdc3
Mark Thompson <mthompson@bitovi.com>
2022-08-16 12:24:21 -0700

    Renamed project

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/npm-registry-analytics'),
+      dir: require('path').join(__dirname, './coverage/opensource-analytics-dashboard'),
       subdir: '.',
       reporters: [
         { type: 'html' },
