97a6322f49bac18c50c48b5bc5ee769fabf8178b
Eduard Krivanek <eduardkrivanek@Eduards-MacBook-Pro-2.local>
2022-09-14 19:33:38 +0300

    fix: removed unsed files

--- src/app/components/search-library/search-library.component.spec.ts
+++ src/app/components/search-library/search-library.component.spec.ts
@@ -3,21 +3,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { SearchLibraryComponent } from './search-library.component';
 
 describe('SearchLibraryComponent', () => {
-  let component: SearchLibraryComponent;
-  let fixture: ComponentFixture<SearchLibraryComponent>;
+	let component: SearchLibraryComponent;
+	let fixture: ComponentFixture<SearchLibraryComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ SearchLibraryComponent ]
-    })
-    .compileComponents();
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			declarations: [SearchLibraryComponent],
+		}).compileComponents();
 
-    fixture = TestBed.createComponent(SearchLibraryComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+		fixture = TestBed.createComponent(SearchLibraryComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
 });
