2aa74b8fdc49aa50f0272e3e62159f4091623c67
Eduard Krivanek <eduardkrivanek@Eduards-MacBook-Pro-2.local>
2022-09-23 21:19:55 +0300

    fix(footer): changed selector and moved styles into styles.scss

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,13 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
-	selector: 'app-footer',
+	// eslint-disable-next-line @angular-eslint/component-selector
+	selector: 'footer',
 	templateUrl: './footer.component.html',
 	styleUrls: ['./footer.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class FooterComponent {
-	constructor() {
-		/* empty */
-	}
-}
+export class FooterComponent {}
