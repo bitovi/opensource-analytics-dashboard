5334fa45e70e18973bef35b834cad03e43e8808b
Mark Thompson <mthompson@bitovi.com>
2022-09-28 22:53:23 -0700

    test: added getParam for params service

--- src/app/services/params/params.service.spec.ts
+++ src/app/services/params/params.service.spec.ts
@@ -4,6 +4,13 @@ import { ParamsService } from './params.service';
 
 describe('ParamsService', () => {
 	let service: ParamsService;
+	// Used to restore window.location
+	const originalLocation = window.location;
+
+	afterEach(() => {
+		// clean up mocks for window.location
+		window.location = originalLocation;
+	});
 
 	beforeEach(() => {
 		TestBed.configureTestingModule({});
@@ -12,5 +19,30 @@ describe('ParamsService', () => {
 
 	it('should be created', () => {
 		expect(service).toBeTruthy();
+	});
+
+	describe('getParam()', () => {
+		it('should return query params if name and value exist in url', () => {
+			Object.defineProperty(window, 'location', {
+				value: new URL('https://website.com?test=a,b,c'),
+				writable: true,
+			});
+			expect(service.getParam('test')).toBe('a,b,c');
+		});
+
+		it('should return null if name and value does NOT exist in url', () => {
+			Object.defineProperty(window, 'location', {
+				value: new URL('https://website.com'),
+				writable: true,
+			});
+			expect(service.getParam('p')).toBeNull();
+		});
+
+		it('should return null if URLSearchParams is NOT supported', () => {
+			jest.spyOn(URLSearchParams.prototype, 'get').mockImplementationOnce(() => {
+				throw new Error('mock error');
+			});
+			expect(service.getParam('p')).toBeNull();
+		});
 	});
 });
