b7da62f2cd68767e5cba717a6493306230637a4e
Wade H <wade@bitovi.com>
2022-11-16 15:47:37 -0500

    AOS2-36 Add tests for DataService

--- src/app/services/data/data.service.spec.ts
+++ src/app/services/data/data.service.spec.ts
@@ -1,5 +1,6 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { format } from 'date-fns';
 
 import { DataService } from './data.service';
 
@@ -17,5 +18,23 @@ describe('NpmRegistryService', () => {
 
 	it('should be created', () => {
 		expect(service).toBeTruthy();
+	});
+
+	describe('isToday()', () => {
+		it('should return true if given todays date in date-fns yyyy-MM-dd format', () => {
+			expect(service.isToday(format(new Date(), 'yyyy-MM-dd'))).toBe(true);
+		});
+		it('should return true if NOT given todays date in date-fns yyyy-MM-dd format', () => {
+			expect(service.isToday('not-today')).toBe(false);
+		});
+	});
+
+	describe('getQuerySlug()', () => {
+		it('should return formatted string', () => {
+			const packageName = 'amazon';
+			const start = 'start';
+			const end = 'end';
+			expect(service.getQuerySlug(packageName, start, end)).toEqual(`${packageName}__${start}__${end}`);
+		});
 	});
 });
